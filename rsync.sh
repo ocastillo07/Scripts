e858cdcd1e4145ef627a5a61888808cb7bcee622
ozkarca <ozkar.ca@gmail.com>
2014-03-05 15:43:23 -0800

    crontab config
    how to add this script to crontab from shell

--- rsync.sh
+++ rsync.sh
@@ -3,3 +3,6 @@
 # Hourly Data Backup
 rsync -az --progress --delete --size-only /FECarpetas/Empresas/ 10.183.136.144:/FECarpetas/Empresas/
 
+####para agregarlo al crontab
+# shell$ crontab -e
+#0 * * * * /root/FESync.sh
